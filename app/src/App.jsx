0ae682c714735f14a5825c90f0fd096c44cb64ee
samane ghodrati <samane.ghodrati@gmail.com>
2025-08-26 23:21:49 -0900

    add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,27 @@
 
 export default function App() {
   return (
-    <div>
-    hi       
+    <div>     
+    <Grid container spacing={2}>
+  <Grid size={{ xs: 6, md: 4 }}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+  <Grid size={{ xs: 6, md: 4 }}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+  <Grid size={{ xs: 6, md: 4 }}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+  <Grid size={{ xs: 6, md: 6 }}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+  <Grid size={{ xs: 6, md: 6 }}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+  <Grid size={12}>
+    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  </Grid>
+</Grid>
     </div>
   );
 }
