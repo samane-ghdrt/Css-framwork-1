9f21780971cc853761afdee0772ec5db4f157004
samane ghodrati <samane.ghodrati@gmail.com>
2025-08-29 16:11:31 -0900

    add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,26 +1,40 @@
-
+import { Button, Grid, TextField, Typography } from "@mui/material";
 export default function App() {
   return (
-    <div>     
-    <Grid container spacing={2}>
-  <Grid size={{ xs: 6, md: 4 }}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+    <div>    
+      <Typography variant="h4" align="center" gutterBottom>
+        Create Your Account
+      </Typography>
+  <Grid container spacing={2}>
+  <Grid item xs={12} md={4}>
+    <TextField id="outlined-basic" label="FullName" variant="outlined" fullWidth/>
   </Grid>
-  <Grid size={{ xs: 6, md: 4 }}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  <Grid item xs={12} md={4}>
+   <TextField id="outlined-basic" label="UserName" variant="outlined" fullWidth/>
   </Grid>
-  <Grid size={{ xs: 6, md: 4 }}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  <Grid item xs={12} md={4}>
+   <TextField id="outlined-basic" label="Age" variant="outlined"fullWidth />
   </Grid>
-  <Grid size={{ xs: 6, md: 6 }}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  <Grid item xs={12} md={6}>
+  <TextField id="outlined-basic" label="email" variant="outlined"fullWidth />
   </Grid>
-  <Grid size={{ xs: 6, md: 6 }}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  <Grid item xs={12} md={6}>
+  <TextField id="outlined-basic" variant="outlined" label="Password" type="password"fullWidth/>
   </Grid>
-  <Grid size={12}>
-    <Item><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Item>
+  <Grid item xs={12} >
+ <TextField sx={{ height: '100%', boxSizing: 'border-box' }} id="outlined-basic" label="Outlined" variant="outlined"    multiline
+            rows={4}
+            fullWidth />
   </Grid>
+
+  <Grid item xs={12} align="center">
+  <Button
+            variant="contained"
+            style={{ backgroundColor: "purple", color: "white", padding: "10px 40px" }}
+          >
+            SIGN UP
+          </Button>
+          </Grid>
 </Grid>
     </div>
   );
